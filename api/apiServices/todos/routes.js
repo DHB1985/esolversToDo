0fefa103d3b15aaa59f7a24380d612f39e3eaabb
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2022-02-15 14:53:53 -0300

    Del comments and console.log

--- api/apiServices/todos/routes.js
+++ api/apiServices/todos/routes.js
@@ -12,7 +12,6 @@ const {
 router.get("/all", getTodos);
 router.post("/", createTodo);
 router.put("/:id", updateTodo);
-//router.put("/", updateTodo);
 router.delete("/:id", deleteTodo);
 router.get("/:id", getTodoById);
 
--- api/services/db.js
+++ api/services/db.js
@@ -1,16 +1,7 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const pg = require("pg");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
-
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-//   {
-//     logging: false,
-//     native: false,
-//   }
-// );
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
